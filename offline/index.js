93f0200c80e8147b11674640eca85433e4128d2f
JOU Amjs <huzhen555@qq.com>
2020-07-16 11:24:33 +0800

    修改占位符

--- offline/index.js
+++ offline/index.js
@@ -1,16 +1,15 @@
 /*
  * @Date: 2020-05-11 10:16:37
  * @LastEditors: JOU(wx: huzhen555)
- * @LastEditTime: 2020-07-10 16:58:58
- */
-class OfflinePluginDesign extends EL.MenuProgramme {
+ * @LastEditTime: 2020-07-16 11:15:35
+ */ 
+class {{ pluginID }} extends EL.MenuProgramme {
   loadMenu() {
     // ...
   }
   orderChange() {
     // ...
-    const a = 23;
   }
 }
 
-mpClass.entry = DemoPlugin;
+mpClass.entry = {{ pluginID }};
