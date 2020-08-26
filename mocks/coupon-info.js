2541fb1e601a16cd301888efed75efb559c15ff9
JOU Amjs <huzhen555@qq.com>
2020-08-26 17:40:28 +0800

    Update coupon-info.js

--- mocks/coupon-info.js
+++ mocks/coupon-info.js
@@ -1,13 +1,14 @@
 /*
  * @Date: 2020-07-18 14:06:49
  * @LastEditors: JOU(wx: huzhen555)
- * @LastEditTime: 2020-07-18 14:12:55
+ * @LastEditTime: 2020-08-25 11:43:31
  */ 
 module.exports = {
-  3: {
-    id: 3,
-    name: '中秋快乐优惠券',
-    createTimestamp: 14847293729,
+  1: {
+    id: 1,
+    name: '中秋快乐优惠券1',
+    expireTime: 15927293729,
+    couponStr: '可抵用10元',
     couponData: {
       money: 10,
     },
@@ -15,12 +16,20 @@ module.exports = {
       { order_amount: 100 },
       { date_type: 'week', date: [6,0] },
     ],
+    useLimitStrArray: [
+      '订单满100元可用',
+      '周六日不可用',
+    ],
     useDays: 60,
+    limitAmount: 1000,
+    receivedAmount: 200,
+    status: 1,
   },
   3: {
     id: 3,
     name: '中秋快乐优惠券',
-    createTimestamp: 14847293729,
+    expireTime: 15927293729,
+    couponStr: '可抵用10元',
     couponData: {
       money: 10,
     },
@@ -28,12 +37,22 @@ module.exports = {
       { order_amount: 100 },
       { date_type: 'week', date: [6,0] },
     ],
+    useLimitStrArray: [
+      '订单满100元可用',
+      '周六日不可用',
+    ],
     useDays: 60,
+    limitAmount: 1000,
+    receivedAmount: 200,
+    limitAmount: 1000,
+    receivedAmount: 200,
+    status: 1,
   },
   4: {
     id: 4,
     name: '中秋快乐优惠券4',
-    createTimestamp: 14847293729,
+    expireTime: 15927293729,
+    couponStr: '可抵用10元',
     couponData: {
       money: 10,
     },
@@ -41,12 +60,22 @@ module.exports = {
       { order_amount: 100 },
       { date_type: 'week', date: [6,0] },
     ],
+    useLimitStrArray: [
+      '订单满100元可用',
+      '周六日不可用',
+    ],
     useDays: 60,
+    limitAmount: 1000,
+    receivedAmount: 200,
+    limitAmount: 1000,
+    receivedAmount: 200,
+    status: 1,
   },
   5: {
     id: 5,
     name: '中秋快乐优惠券5',
-    createTimestamp: 14847293729,
+    couponStr: '可抵用10元',
+    expireTime: 15927293729,
     couponData: {
       money: 10,
     },
@@ -54,6 +83,13 @@ module.exports = {
       { order_amount: 100 },
       { date_type: 'week', date: [6,0] },
     ],
+    useLimitStrArray: [
+      '订单满100元可用',
+      '周六日不可用',
+    ],
     useDays: 60,
+    limitAmount: 1000,
+    receivedAmount: 200,
+    status: 1,
   },
 }
